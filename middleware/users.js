a2bd674805e385ae1d7ef958414ef327dddf8745
Luis <lvalladares@ucol.mxm>
2022-05-27 16:00:59 -0500

    fisrt commit

---
+++ middleware/users.js
@@ -0,0 +1,52 @@
+const req = require("express/lib/request");
+const res = require("express/lib/response");
+const jwt = require("jsonwebtoken");
+
+module.exports = {
+  validateRegister: (req, res, next) => {
+    // username min length 3
+    if (!req.body.username || req.body.username.length < 3) {
+      return res.status(400).send({
+        msg: 'Please enter a username with min. 3 chars'
+      });
+    }
+
+    // password min 6 chars
+    if (!req.body.password || req.body.password.length < 6) {
+      return res.status(400).send({
+        msg: 'Please enter a password with min. 6 chars'
+      });
+    }
+
+    // password (repeat) does not match
+    if (
+      !req.body.password_repeat ||
+      req.body.password != req.body.password_repeat
+    ) {
+      return res.status(400).send({
+        msg: 'Both passwords must match'
+      });
+    }
+
+    next();
+  },
+    isLoggedIn: (req, res, next) => {
+         if(!req.headers.authorization){
+            return res.status(400).send({
+                msg: "your session is not valid", 
+            });
+         }
+        try {
+            const authHeader = req.headers.authorization;
+            const token = authHeader.split('')[1];
+            const decoded = jwt.verify(token, 'SECRETKEY');
+            req.userData = decoded;
+            next();
+        } catch(err) {
+            throw err;
+            return res.status(400).send({
+                msg: "your session is not valid", 
+            });
+        }
+    },
+};
